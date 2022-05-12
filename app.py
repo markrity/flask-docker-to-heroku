1b223f4b16c4cd9c409dbd392cd3df82791e5d4a
markrity <11563691+markrity@users.noreply.github.com>
2022-05-12 10:21:51 +0300

    change hello

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from os import environ
 app = Flask(__name__)
 @app.route('/')
 def hello():
-  return "Hello World!"
+  return "Hello World123!"
 if __name__ == '__main__':
   app.run(debug = True, host = '0.0.0.0', port=environ.get("PORT", 5000))
