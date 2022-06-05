a9c8f6d3d3aeca115b0343f51175805960749f5c
markrity <11563691+markrity@users.noreply.github.com>
2022-06-05 18:22:01 +0300

    return header

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from os import environ
 app = Flask(__name__)
 @app.route('/')
 def hello():
-  return "Hello SCE!"
+  return "<h1>Hello World</h1>"
 if __name__ == '__main__':
   app.run(debug = True, host = '0.0.0.0', port=environ.get("PORT", 5000))
