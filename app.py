36239e88ec95fd40f56ef500331d53402ce07be2
markrity <11563691+markrity@users.noreply.github.com>
2022-06-05 18:42:39 +0300

    add hello sce

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from os import environ
 app = Flask(__name__)
 @app.route('/')
 def hello():
-  return "<h1>Hello World</h1>"
+  return "<h1>Hello SCE!</h1>"
 if __name__ == '__main__':
   app.run(debug = True, host = '0.0.0.0', port=environ.get("PORT", 5000))
